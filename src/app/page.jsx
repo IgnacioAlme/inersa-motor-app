8b323dfda40daf8ae510636e9a2c9f2054bb71a2
IgnacioAlme <ignacio.yair.almenar@gmail.com>
2024-10-10 16:32:03 -0300

    Calendario

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,12 @@
-// pages/index.jsx
-
 import Link from 'next/link';
+import AppointmentCalendar from '../components/AppointmentCalendar';
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <main className="flex-grow container mx-auto p-6">
         <h2 className="text-xl font-semibold mb-4">Navegación</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
           <Link legacyBehavior href="/clientes">
             <a className="block p-6 bg-white rounded-lg shadow hover:shadow-lg transition">
               <h3 className="text-lg font-bold">Clientes</h3>
@@ -28,11 +27,14 @@ export default function Home() {
           </Link>
           <Link legacyBehavior href="/revisiones">
             <a className="block p-6 bg-white rounded-lg shadow hover:shadow-lg transition">
-              <h3 className="text-lg font-bold">Revisiones</h3>
-              <p className="mt-2">Ver la lista de revisiones</p>
+              <h3 className="text-lg font-bold">Informes</h3>
+              <p className="mt-2">Acerca de últimas revisiones</p>
             </a>
           </Link>
         </div>
+        
+        <h2 className="text-xl font-semibold mb-4">Calendario de Citas</h2>
+        <AppointmentCalendar />
       </main>
     </div>
   );
